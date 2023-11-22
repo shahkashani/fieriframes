ecbc7ed542d4f4bb7d191b27b8dbd28f5f8b45b1
Shah Kashani <skashani@gmail.com>
2023-11-22 16:49:28 +0100

    Trails

--- configs/utils/get-filters.js
+++ configs/utils/get-filters.js
@@ -4,4 +4,5 @@ module.exports = () => ({
   grayscale: () => new stills.filters.Grayscale(),
   reverse: () => new stills.filters.Reverse(),
   jitter: () => new stills.filters.Jitter(),
+  trails: () => new stills.filters.Trails(),
 });
